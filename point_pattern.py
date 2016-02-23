ca768d542ab4b06514ec42882eef85ae235182b8
Adam Herczeg <herczegadam@yahoo.com>
2016-02-23 12:31:37 -0700

    Fourth

--- point_pattern.py
+++ point_pattern.py
@@ -135,21 +135,7 @@ def average_nearest_neighbor_distance(points):
     """
 
     mean_d = 0
-    neighbor = None
-    for point in points:
-        if:
-            point_a != point_b
-            thisDistance = euclidean_distance(point_a, point_b)
-            if:
-                neighbor = None
-                neighbor = thisDistance
-                    
-        else:
-            neighbor > thisDistance
-                neighbor = thisDistance
-        mean_d += neighbor
-        neighbor = None
-        mean_d/=len(points)
+   
 
     return mean_d
 
