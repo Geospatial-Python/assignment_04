0ffddc08e0ad43342b588e05d863056d1be9fbfa
thervig <thervig@asu.edu>
2016-02-22 23:24:35 -0700

    Update point_pattern.py

--- point_pattern.py
+++ point_pattern.py
@@ -5,7 +5,6 @@ Like last assignment, we are going to be working with point
 patterns.  The readings focused on iteration, sequences, and
 conditional execution.  We are going to use these concepts
 to write functions to:
-
 1. Read a geojson file
 2. Parse a geojson file to find the largest city by population
 3. Write your own code to do something interesting with the geojson
@@ -20,12 +19,10 @@ to write functions to:
 def read_geojson(input_file):
     """
     Read a geojson file
-
     Parameters
     ----------
     input_file : str
                  The PATH to the data to be read
-
     Returns
     -------
     gj : dict
@@ -33,7 +30,9 @@ def read_geojson(input_file):
     """
     # Please use the python json module (imported above)
     # to solve this one.
-    gj = None
+    with open ('data/us_cities.geojson', 'r') as f:
+        gj = json.load(f)
+
     return gj
 
 
@@ -42,22 +41,23 @@ def find_largest_city(gj):
     Iterate through a geojson feature collection and
     find the largest city.  Assume that the key
     to access the maximum population is 'pop_max'.
-
     Parameters
     ----------
     gj : dict
          A GeoJSON file read in as a Python dictionary
-
     Returns
     -------
     city : str
            The largest city
-
     population : int
                  The population of the largest city
     """
-    city = None
-    max_population = 0
+    temp = 0
+    for i in gj:
+       if i['pop_max'] > temp:
+            temp = i['pop_max']
+            city = i['city']
+            max_population = temp
 
     return city, max_population
 
@@ -66,35 +66,44 @@ def write_your_own(gj):
     """
     Here you will write your own code to find
     some attribute in the supplied geojson file.
-
     Take a look at the attributes available and pick
     something interesting that you might like to find
     or summarize.  This is totally up to you.
-
     Do not forget to write the accompanying test in
     tests.py!
     """
-    return
+    temp = 0
+    for i in gj:
+        if i['pop_min'] < temp:
+            temp = i['pop_min']
+            city = i['city']
+            min_population = temp
+
+    return city, min_population
 
 def mean_center(points):
     """
     Given a set of points, compute the mean center
-
     Parameters
     ----------
     points : list
          A list of points in the form (x,y)
-
     Returns
     -------
     x : float
         Mean x coordinate
-
     y : float
         Mean y coordinate
     """
-    x = None
-    y = None
+
+    summation_x = 0
+    summation_y = 0
+    for i in points:
+        summation_x += i[0]
+        summation_y += i[1]
+
+    x = float(summation_x / len(points))
+    y = float(summation_y / len(points))
 
     return x, y
 
@@ -102,24 +111,23 @@ def mean_center(points):
 def average_nearest_neighbor_distance(points):
     """
     Given a set of points, compute the average nearest neighbor.
-
     Parameters
     ----------
     points : list
              A list of points in the form (x,y)
-
     Returns
     -------
     mean_d : float
              Average nearest neighbor distance
-
     References
     ----------
     Clark and Evan (1954 Distance to Nearest Neighbor as a
      Measure of Spatial Relationships in Populations. Ecology. 35(4)
      p. 445-453.
     """
-    mean_d = 0
+
+
+    mean_d = mean_center(points) / len(points)
 
     return mean_d
 
@@ -127,19 +135,22 @@ def average_nearest_neighbor_distance(points):
 def minimum_bounding_rectangle(points):
     """
     Given a set of points, compute the minimum bounding rectangle.
-
     Parameters
     ----------
     points : list
              A list of points in the form (x,y)
-
     Returns
     -------
      : list
        Corners of the MBR in the form [xmin, ymin, xmax, ymax]
     """
 
-    mbr = [0,0,0,0]
+    xmin = min(points[0])
+    ymin = min(points[1])
+    xmax = max(points[0])
+    ymax = max(points[1])
+
+    mbr = [xmin,ymin,xmax,ymax]
 
     return mbr
 
@@ -148,7 +159,7 @@ def mbr_area(mbr):
     """
     Compute the area of a minimum bounding rectangle
     """
-    area = 0
+    area = (mbr[2] - mbr[0]) * (mbr[3] - mbr[1])
 
     return area
 
@@ -157,23 +168,20 @@ def expected_distance(area, n):
     """
     Compute the expected mean distance given
     some study area.
-
     This makes lots of assumptions and is not
     necessarily how you would want to compute
     this.  This is just an example of the full
     analysis pipe, e.g. compute the mean distance
     and the expected mean distance.
-
     Parameters
     ----------
     area : float
            The area of the study area
-
     n : int
         The number of points
     """
 
-    expected = 0
+    expected = .5 / ((math.sqrt(n/area)))
     return expected
 
 
@@ -188,15 +196,12 @@ the assignment
 def manhattan_distance(a, b):
     """
     Compute the Manhattan distance between two points
-
     Parameters
     ----------
     a : tuple
         A point in the form (x,y)
-
     b : tuple
         A point in the form (x,y)
-
     Returns
     -------
     distance : float
@@ -209,18 +214,14 @@ def manhattan_distance(a, b):
 def euclidean_distance(a, b):
     """
     Compute the Euclidean distance between two points
-
     Parameters
     ----------
     a : tuple
         A point in the form (x,y)
-
     b : tuple
         A point in the form (x,y)
-
     Returns
     -------
-
     distance : float
                The Euclidean distance between the two points
     """
@@ -231,28 +232,21 @@ def euclidean_distance(a, b):
 def shift_point(point, x_shift, y_shift):
     """
     Shift a point by some amount in the x and y directions
-
     Parameters
     ----------
     point : tuple
             in the form (x,y)
-
     x_shift : int or float
               distance to shift in the x direction
-
     y_shift : int or float
               distance to shift in the y direction
-
     Returns
     -------
     new_x : int or float
             shited x coordinate
-
     new_y : int or float
             shifted y coordinate
-
     Note that the new_x new_y elements are returned as a tuple
-
     Example
     -------
     >>> point = (0,0)
@@ -275,10 +269,8 @@ def check_coincident(a, b):
     ----------
     a : tuple
         A point in the form (x,y)
-
     b : tuple
         A point in the form (x,y)
-
     Returns
     -------
     equal : bool
@@ -290,12 +282,10 @@ def check_coincident(a, b):
 def check_in(point, point_list):
     """
     Check whether point is in the point list
-
     Parameters
     ----------
     point : tuple
             In the form (x,y)
-
     point_list : list
                  in the form [point, point_1, point_2, ..., point_n]
     """
@@ -307,12 +297,10 @@ def getx(point):
     A simple method to return the x coordinate of
      an tuple in the form(x,y).  We will look at
      sequences in a coming lesson.
-
     Parameters
     ----------
     point : tuple
             in the form (x,y)
-
     Returns
     -------
      : int or float
@@ -326,12 +314,10 @@ def gety(point):
     A simple method to return the x coordinate of
      an tuple in the form(x,y).  We will look at
      sequences in a coming lesson.
-
     Parameters
     ----------
     point : tuple
             in the form (x,y)
-
     Returns
     -------
      : int or float
